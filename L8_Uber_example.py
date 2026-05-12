be60923dcad14b704440fa8e23bfc35041b73068
sba25309-source <sba25309@student.cct.ie>
2026-05-12 19:26:32 +0100

    Update L8_Uber_example.py

--- L8_Uber_example.py
+++ L8_Uber_example.py
@@ -12,7 +12,7 @@ st.title("Uber Pickups in New York")
 
 
 #st.bar_chart(df, x="category", y="sales")
-col1, col2 = st.columns([3, 1])
+col1, col2 = st.columns([2, 2])
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
